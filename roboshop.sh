4fafd24efd817ef5a6837eec78602108e2755622
lokesh <lokesh.theofficial@gmail.com>
2026-05-16 10:17:14 +0530

    some change

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-0a3d6accff7702558"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0558625374KS8URP4XXN"
+DOMAIN_NAME="lokeshinfo.online"
 
 for instance in $@
 do
@@ -20,6 +22,7 @@ do
              --query 'Reservations[].Instances[].PublicIpAddress' \
              --output text
         )
+        RECORD_NAME="$DOMAIN_NAME" #lokeshinfo.online
     else
         IP=$(
             aws ec2 describe-instances \
@@ -27,6 +30,32 @@ do
              --query 'Reservations[].Instances[].PrivateIpAddress' \
              --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.lokeshinfo.online
     fi
-   echo "IP Adress: $IP"
+   echo "IP Address: $IP"
+
+   aws route53 change-resource-record-sets \
+   --hosted-zone-id $ZONE_ID \
+   --change-batch '
+   {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+   '
+    echo "record updated for $instance"
+
 done
